67dde1e3f4a790ddf94819a93f04ce36f791ec0b
Lifu Song <songlf@tib.cas.cn>
2026-04-22 21:57:05 +0800

    Add Python dependency requirements and update decode configuration
    - Create requirements.txt with external package dependencies (numpy, pyDes, khmer)
    - Update decode.py with modified parameters for NGS data processing
      - Adjust delta to 0.1 and c_value to 0.2 for robust distribution
      - Configure input file path for 20260408-NGS-Maqiang dataset
      - Set kmer_size to 27 and chunk_num to 3142
      - Update output file extension to .ECC format
    - Minor adjustments to deBruijnGraph.py
    
    Dependencies documented:
      * numpy>=1.19.0: Numerical operations and array handling
      * pyDes>=2.0.1: DES encryption for data security
      * khmer>=2.1.1: K-mer counting for De Bruijn graph construction

--- decode.py
+++ decode.py
@@ -6,147 +6,178 @@ from deBruijnGraph import DeBruijnGraph
 from glass import Glass
 from DNAdroplet import DNADroplet
 import time
-import os.path
-import getopt
+# import os.path
+# import getopt
+
+#####
+# chunk_size = 35
+# fountain_seed = 2222
+# droplet_num = 2000
+# double_index = False
+# start_index = 1
+# work_dir = r''
+# input_file = ''
+# output_file = ''  # Will be set to input_file + ".fasta" if not specified
+# index_bytes = 4
+# anchor_bytes = 4
+# ec_bytes = 2
+# redundancy_rate = 0
+# test_num = 0
+# test_dropout_rate = 0.05
+#####
 
 
 
-input_file = ''
-file_type = 'dump_kmers'
-output_file = 'output.rar'
-kmer_size = 31
-kmer_cut_off = 0
+delta = 0.1
+c_value = 0.2
+
+input_file = r'/data/songlf/0.DNA_Storage/0.GenomePreservation/20260408-NGS-Maqiang/X101SC26038058-Z01/X101SC26038058-Z01-J001/01.RawData/A/A_12.fq'
+
+# file_type = 'dump_kmers'
+output_file = 'output.ecc'
+
+kmer_size = 27
+kmer_cut_off = 5
+output_file = str(kmer_cut_off) + '.ECC'
 chunk_size = 35
-chunk_num = 85431
-f_seed = 1
+
+chunk_num = 3142
+fountain_seed = 2222
 index_bytes = 4
 crc_bytes = 2
-double_index = False
-both_way = True
+# double_index = False
+# both_way = True
 index_l = 1
 index_u = (chunk_num * 2) + index_l
 
-opts,args = getopt.getopt(sys.argv[1:],'-h-i:-o:t:-k:-c:-n:-s:-a-b',
-                          ['help','input=','output=', 'file_type=', 'kmer_size=', 'chunk_size=', 'chunk_num=', 'seed=', 'anchor', 'both_way', 'min_index=',  'max_index=', 'index_bytes=', 'ec_bytes='])
+# sec_key = 0b10101010101111011001010100110110
+# # sec_key = 0b10101010101111011001010100110111 # wrong key
+# sec_key = sec_key.to_bytes(8, byteorder ='big')
 
-usage = 'Usage:\n' + r'      python decode.py -i input_file -t type_of_seqs -o outfile [Options]'
-options = 'Options:\n'
-options = options + r'      -h, --help                              Show help information' + '\n'
-options = options + r'      -i, --input   <input file>              Input file' + '\n'
-options = options + r'      -t, --file_type   <file type>           Input file type: FastQ, Fasta or Jellyfish dumped k-mers (default)' + '\n'
-options = options + r'      -o, --output  <output file>             Output file' + '\n'
-options = options + r'      -k, --kmer_size  <number>               k-mer size, default = 21 ' + '\n'
-options = options + r'      -c, --chunk_size  <size>                Chunk size, default = 30 (bytes)' + '\n'
-options = options + r'      -n, --chunk_num  <number>               Chunk number, default = 10,740 (for testing only)' + '\n'
-options = options + r'      --cut  <number>                         Cut_off for elimination of low coverage k-mers, default = 0 ' + '\n'
-options = options + r'      -s, --seed    <seed>                    Fountain random seed, default 1' + '\n'
-options = options + r'      -a, --anchor                            Anchor codes, default On' + '\n'
-options = options + r'      -b, --both_way                          Both-way search mode, default On' + '\n'
-options = options + r'      --min_index  <initial index>            Initial index, default = 1' + '\n'
-options = options + r'      --max_index  <max index>                Max index, default = 20000' + '\n'
-options = options + r'      --index_bytes  <number>                 Length of index and anchor codes, default = 4 (bytes)' + '\n'
-options = options + r'      --ec_bytes  <number>                    Length of ec codes, default = 2 (bytes)' + '\n'
+# droplet_template = droplet_tp()
+def droplet_tp():
+    adp = DNADroplet(bytes(chunk_size))
+    adp.des = False
+    # adp.sec_key = sec_key
+    adp.num_of_chunks = chunk_num
+    return adp
 
 
-for opt_name,opt_value in opts:
-    if opt_name in ('-h','--help'):
-        print(usage)
-        print(options)
-        sys.exit()
-    if opt_name in ('-i','--input'):
-        input_file = opt_value
-    if opt_name in ('-o','--output'):
-        output_file = opt_value
-    if opt_name in ('-t','--file_type'):
-        file_type = opt_value
-    if opt_name in ('-k','--kmer_size'):
-        kmer_size = int(opt_value)
-    if opt_name in ('-c','--chunk_size'):
-        chunk_size = int(opt_value)
-    if opt_name in ('-n', '--chunk_num'):
-        chunk_num = int(opt_value)
-    if opt_name in ('-s', '--seed'):
-        f_seed = int(opt_value)
-    if opt_name in ('-a', '--anchor'):
-        double_index = True
-    if opt_name in ('--min_index', '--notmatch'):
-        index_l = int(opt_value)
+#
+#
+# opts,args = getopt.getopt(sys.argv[1:],'-h-i:-o:t:-k:-c:-n:-s:-a-b',
+#                           ['help','input=','output=', 'file_type=', 'kmer_size=', 'chunk_size=', 'chunk_num=', 'seed=', 'anchor', 'both_way', 'min_index=',  'max_index=', 'index_bytes=', 'ec_bytes='])
+#
+# usage = 'Usage:\n' + r'      python decode.py -i input_file -t type_of_seqs -o outfile [Options]'
+# options = 'Options:\n'
+# options = options + r'      -h, --help                              Show help information' + '\n'
+# options = options + r'      -i, --input   <input file>              Input file' + '\n'
+# options = options + r'      -t, --file_type   <file type>           Input file type: FastQ, Fasta or Jellyfish dumped k-mers (default)' + '\n'
+# options = options + r'      -o, --output  <output file>             Output file' + '\n'
+# options = options + r'      -k, --kmer_size  <number>               k-mer size, default = 21 ' + '\n'
+# options = options + r'      -c, --chunk_size  <size>                Chunk size, default = 30 (bytes)' + '\n'
+# options = options + r'      -n, --chunk_num  <number>               Chunk number, default = 10,741 (for testing only)' + '\n'
+# options = options + r'      --cut  <number>                         Cut_off for elimination of low coverage k-mers, default = 0 ' + '\n'
+# options = options + r'      -s, --seed    <seed>                    Fountain random seed, default 1' + '\n'
+# # options = options + r'      -a, --anchor                            Anchor codes, default On' + '\n'
+# # options = options + r'      -b, --both_way                          Both-way search mode, default On' + '\n'
+# options = options + r'      --min_index  <initial index>            Initial index, default = 1' + '\n'
+# options = options + r'      --max_index  <max index>                Max index, default = 20000' + '\n'
+# options = options + r'      --index_bytes  <number>                 Length of index and anchor codes, default = 4 (bytes)' + '\n'
+# options = options + r'      --ec_bytes  <number>                    Length of ec codes, default = 4 (bytes)' + '\n'
 
-    if opt_name in ('--max_index', '--notmatch'):
-        index_u = int(opt_value)
-
-    if opt_name in ('--index_bytes', '--notmatch'):
-        index_bytes = int(opt_value)
-
-    if opt_name in ('--crc_bytes', '--notmatch'):
-        crc_bytes = int(opt_value)
+#
+# for opt_name,opt_value in opts:
+#     if opt_name in ('-h','--help'):
+#         print(usage)
+#         print(options)
+#         sys.exit()
+#     if opt_name in ('-i','--input'):
+#         input_file = opt_value
+#     if opt_name in ('-o','--output'):
+#         output_file = opt_value
+#     if opt_name in ('-t','--file_type'):
+#         file_type = opt_value
+#     if opt_name in ('-k','--kmer_size'):
+#         kmer_size = int(opt_value)
+#     if opt_name in ('-c','--chunk_size'):
+#         chunk_size = int(opt_value)
+#     if opt_name in ('-n', '--chunk_num'):
+#         chunk_num = int(opt_value)
+#     if opt_name in ('-s', '--seed'):
+#         f_seed = int(opt_value)
+#     # if opt_name in ('-a', '--anchor'):
+#     #     double_index = True
+#     if opt_name in ('--min_index', '--notmatch'):
+#         index_l = int(opt_value)
+#
+#     if opt_name in ('--max_index', '--notmatch'):
+#         index_u = int(opt_value)
+#
+#     if opt_name in ('--index_bytes', '--notmatch'):
+#         index_bytes = int(opt_value)
+#
+#     if opt_name in ('--crc_bytes', '--notmatch'):
+#         crc_bytes = int(opt_value)
 
 
 start = time.perf_counter()
 
-
 deG = DeBruijnGraph()
-deG.kmer_len = kmer_size
-deG.veri_kmers = False
-deG.max_path_num = 10000
+deG.set_kmer_len(kmer_size)
+deG.min_cov = kmer_cut_off + 1
+deG.max_path_num = 1000000
 
-if file_type == 'FastQ' or file_type == 'fastq':
-    deG.open_fastq(input_file)
-else:
-    if file_type == 'fasta' or file_type == 'Fasta':
-        deG.open_fasta(input_file)
-    else:
-        deG.open_dump(input_file)
+print('\nCounting k-mers ......')
+deG.count_file(input_file)
 
-strands_recovered = {}
-strand_search_details = {}
-strand_search_details['crc_fail'] = 0
+# deG.kmer_len = kmer_size
+# deG.veri_kmers = False
+# deG.max_path_num = 10000
 
-print('\nRemoving low coverage k-mers ......')
-deG.remove_low_cov_kmers(kmer_cut_off)
+# if file_type == 'FastQ' or file_type == 'fastq':
+#     deG.open_fastq(input_file)
+# else:
+#     if file_type == 'fasta' or file_type == 'Fasta':
+#         deG.open_fasta(input_file)
+#     else:
+#         deG.open_dump(input_file)
 
-print('\nReconstructing DNA strands ......')
+print('\nReconstructing DNA droplets ......')
+
+degree_table = get_degrees(chunk_num, chunk_num * 3, fountain_seed, delta, c_value)
 
 a = time.perf_counter()
-for index in range(index_l, index_u+1):
-    deG.find_droplet_DNA(index, chunk_size)
-    if len(deG.crc_paths) > 1:
-        strand_search_details['crc_fail']  = strand_search_details['crc_fail']  + 1
-    else:
-        if len(deG.crc_paths) > 0:
-            strands_recovered[index] = deG.crc_paths[0]
-
-a = time.perf_counter() - a
-# print('CRC fails:')
-# print(strand_search_details['crc_fail'])
-print('Recovered: ', end='')
-print(len(strands_recovered), end= '')
-print(' strands used ', end='')
-print(a, end=' seconds\n')
-
-
-print('Rebuilding DNA droplets........')
-degree_table = get_degrees(chunk_num, chunk_num*5, f_seed)
 cup = Glass(chunk_num)
-for id in strands_recovered:
-    adp = DNADroplet(bytes(chunk_size))
-    adp.num_of_chunks = chunk_num
-    adp.set_head_index(id)
-    adp.set_droplet_from_DNA_CRC(strands_recovered[id])
-    adp.degree = degree_table[id-1]
-    adp.update()
-    cup.addDroplet(adp)
 
+droplet_template = droplet_tp()
+for index in range(index_l, index_u+1):
 
+    fd_droplets = deG.find_droplets(index, droplet_template)
+    #print(len(fd_droplets), end=" ")
+    if len(fd_droplets) == 1:
+        fd_droplets[0].degree = degree_table[index % len(degree_table)]
+        fd_droplets[0].update()
+        cup.addDroplet(fd_droplets[0])
+        if len(cup.droplets) % 100 < 1:
+            print("Reconstructed droplet number: " + str(len(cup.droplets)))
+    else:
+        if len(fd_droplets) > 1:
+            print("Multiple droplets found with index " + str(index))
 
+hashToFile2(deG.path_sta, output_file + "cut_sta")
 
 print('Decoding by fountain codes .........')
 
 cup.decode()
-cup.writeToFile(output_file)
+if cup.isDone():
+    cup.writeToFile(output_file)
+else:
+    print("Failed to decode the original information.")
 
 end = time.perf_counter() - start
-print('Dncoding time: ', end ='')
+print('Decoding time: ', end ='')
 print(end)
+
 
 
